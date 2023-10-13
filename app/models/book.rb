225e4505f5ead4cb6fd314e52d3a1658a287704f
HisashiSaeki <143866672+HisashiSaeki@users.noreply.github.com>
2023-10-13 00:56:18 +0000

    [Update]直近1週間のいいね多い順　修正

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,8 +3,8 @@ class Book < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
-  validates :title,presence:true
-  validates :body,presence:true,length:{maximum:200}
+  validates :title, presence:true
+  validates :body, presence:true, length:{maximum:200}
 
   def favorited_by?(user)
     favorites.exists?
